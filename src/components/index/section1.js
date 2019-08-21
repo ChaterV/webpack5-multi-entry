8eaa46d2477533312d8b497bde85fcff498ac187
chaterv <yz455699514@gmail.com>
2019-08-21 17:40:19 +0800

    fixed

--- src/components/index/section1.js
+++ src/components/index/section1.js
@@ -1,3 +1,5 @@
+import Vue from 'vue'
+
 Vue.component('section1', {
     template: (
         `<div>
--- src/components/index/section3.js
+++ src/components/index/section3.js
@@ -1,3 +1,5 @@
+import Vue from 'vue'
+
 Vue.component('section3', {
     template: (
         `<div>
