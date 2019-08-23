2b393b973da8e89451ead551bbb902f60d24ac9a
chaterv <yz455699514@gmail.com>
2019-08-23 17:11:43 +0800

    修复默认 index 入口配置

--- build/entry-config.js
+++ build/entry-config.js
@@ -6,18 +6,18 @@
 // 模块的文件夹名作为 key (模块名)
 // 不指定 html 模板，每个文件夹下就一个 html 文件
 const ENTRY_CONF = {
-  'index': {
-    // html: 'index.html',
-    js: 'index.js'
-  },
-  'page1': {
-    // html: 'page1.html',
-    js: 'page1.js'
-  },
-  'page2': {
-    // html: 'page2.html',
-    // js: 'index.js'
-  },
+  // 'index': {
+  //   // html: 'index.html',
+  //   js: 'index.js'
+  // },
+  // 'page1': {
+  //   // html: 'page1.html',
+  //   js: 'page1.js'
+  // },
+  // 'page2': {
+  //   // html: 'page2.html',
+  //   // js: 'index.js'
+  // },
 }
 
 module.exports.ENTRY_CONF = ENTRY_CONF
