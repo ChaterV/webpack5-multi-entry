7b7cb42a1a7a6a9c7dda349d077dabf036241bca
chaterv <yz455699514@gmail.com>
2019-08-28 11:11:49 +0800

    默认本地 ip 访问

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -2,11 +2,32 @@ const path = require('path')
 const merge = require("webpack-merge")
 const webpackConfigBase = require('./webpack.base.conf')
 const webpack = require("webpack")
+const os = require('os')
+
 
 const EC = require("./entry-config")
 const devOpenPage = EC.DEV_OPEN_PAGE  // 配置 run dev 默认打开哪个页面
 
-// console.log('--------,',process.env.BASE_URL)
+
+function getNetworkIp() {
+	let needHost = ''
+	try {
+		// 获得网络接口列表
+		let network = os.networkInterfaces()
+		for (let dev in network) {
+			let iface = network[dev]
+			for (let i = 0; i < iface.length; i++) {
+				let alias = iface[i]
+				if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+					needHost = alias.address
+				}
+			}
+		}
+	} catch (e) {
+		needHost = 'localhost'
+	}
+	return needHost
+}
 const webpackConfigDev = {
 	mode: 'development',
 	output: {
@@ -18,7 +39,7 @@ const webpackConfigDev = {
     contentBase: path.join(__dirname, "../src/pages/index"),
     openPage: devOpenPage || 'index.html', // 配置 run dev 默认打开哪个页面
 		publicPath:'/',
-		host: "0.0.0.0",
+		host: getNetworkIp(),
 		port: "8787",
 		overlay: true, // 浏览器页面上显示错误
 		open: true, // 开启浏览器
