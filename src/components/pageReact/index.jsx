2966540fe2010bebd3300bbfab1a9d257edd95d7
chaterV <455699514@qq.com>
2021-12-30 15:58:23 +0800

    - 依赖更新

---
+++ src/components/pageReact/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PageReact = () => {
+    return (
+        <div className='w-full text-center'>
+            <div>这是 React 页面</div>
+            <button className="mt-[10px] bg-[#3a7bff] p-[10px] text-[#fff] rounded-[5px] hover:bg-[#1c63c9] transform-gpu transition-all" onClick={() => window.location.href = 'index.html'}>to VuePage</button>
+        </div>
+    )
+}
+
+export default PageReact
